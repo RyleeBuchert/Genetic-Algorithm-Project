80ea37575d6aea22759ab871bc5cf99ad7cc308c
RyleeBuchert <ryleebuchert@gmail.com>
2022-03-21 22:49:14 -0500

    Setup

---
+++ genetic_algorithm.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+
+class Chromosome:
+
+    def __init__(self):
+        pass
+
+    def score_chromosome(self):
+        pass
+
+
+class GeneticAlgorithm:
+
+    def __init__(self, selection, crossover, mutation, mutation_rate):
+        pass
+
+
+if __name__ == "__main__":
+
+    print()
